45010f3353b47b1b5828003e94a88456de073ea7
Owen Cruse <ocruse7@gmail.com>
2019-11-09 23:08:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else
+    return FALSE
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+  (speed <= 60) && (speed >= 40) ? false : true
 end
-	
-
 
